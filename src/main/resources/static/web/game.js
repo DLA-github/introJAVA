2a6d2bb6866b8a4616280a0912be1613d6751bfd
David Lucas <david.lucas.a@gmail.com>
2019-08-05 13:26:23 +0200

    2nd try

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -71,7 +71,7 @@ var game = new Vue({
         keys: [],
         enemyTurn: 0,
         ready: false,
-        popup: false
+
     },
 
     created() {
@@ -440,8 +440,30 @@ var game = new Vue({
                     } else if (data.message) {
                         let message = data.message;
                         if (message.includes("Game")) {
-                            //alert(message);
-                            this.popup = true;
+                            var modal = document.getElementById("myModal");
+
+                            // Get the button that opens the modal
+                            var btn = document.getElementById("myBtn");
+
+                            // Get the <span> element that closes the modal
+                            var span = document.getElementsByClassName("close")[0];
+
+                            // When the user clicks the button, open the modal 
+                            btn.onclick = function () {
+                                modal.style.display = "block";
+                            }
+
+                            // When the user clicks on <span> (x), close the modal
+                            span.onclick = function () {
+                                modal.style.display = "none";
+                            }
+
+                            // When the user clicks anywhere outside of the modal, close it
+                            window.onclick = function (event) {
+                                if (event.target == modal) {
+                                    modal.style.display = "none";
+                                }
+                            }
                         } else {
                             this.sendSocket();
                             this.sendSalvo = true;
