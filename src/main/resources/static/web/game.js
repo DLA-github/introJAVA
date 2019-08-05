f8bef144ffa96ebc3c8ea75e3950fc3906cb9453
David Lucas <david.lucas.a@gmail.com>
2019-08-05 13:34:57 +0200

    2nd try

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -448,10 +448,9 @@ var game = new Vue({
                             // Get the <span> element that closes the modal
                             var span = document.getElementsByClassName("close")[0];
 
-                            // When the user clicks the button, open the modal 
-                            btn.onclick = function () {
-                                modal.style.display = "block";
-                            }
+
+                            //shows pop Up
+                            modal.style.display = "block";
 
                             // When the user clicks on <span> (x), close the modal
                             span.onclick = function () {
